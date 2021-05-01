510b4c007c88cff213c272508a088748b50bc71b
Lucas Viana <oviana@outlook.com>
2021-05-01 20:18:23 -0300

    ex02 done

--- ex02/main.c
+++ ex02/main.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// csv columns: ID (null?) - NAME - AGE
 
 int main(){
     // .csv filename string (expecting max 20 chars)
@@ -11,18 +7,17 @@ int main(){
     // reads filename from stdin
     scanf("%s", filename);
 
-    // opens the file in reading mode
+    // opens the file in binary-reading mode
     FILE *fp = fopen(filename, "rb");
 
-    // temporary/auxiliar variables to read each .csv row
-    // and corresponding csvs
+    // temporary/auxiliar variables to read each value
     char tmp_name[50]; 
     int tmp_id, tmp_age;
     
     // loops through every register
-    while(fread(tmp_id, sizeof(int), 1, fp) && fread(tmp_name, sizeof(char), 50, fp) && fread(tmp_age, sizeof(int), 1, fp)){
-        // if that row's user is overage, prints output
-        if(tmp_age >= 18){ strcmp(tmp_name, "") ? printf("O usuario %s eh de maior.\n", tmp_name) : printf("O usuario de identificacao %s eh de maior.\n", tmp_id); }
+    while(fread(&tmp_id, sizeof(int), 1, fp) && fread(tmp_name, sizeof(char), 50, fp) && fread(&tmp_age, sizeof(int), 1, fp)){
+        // if that user is overage, prints output
+        if(tmp_age >= 18){ (tmp_name[0] != '\0') ? printf("O usuario %s eh de maior.\n", tmp_name) : printf("O usuario de identificacao %d eh de maior.\n", tmp_id); }
     }
     
     fclose(fp);
