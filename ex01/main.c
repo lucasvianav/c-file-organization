822c423ab39e1ea6b0c0bc9dcd7209ce5559c005
Lucas Viana <oviana@outlook.com>
2021-05-01 17:48:46 -0300

    ex01 complete - splits rows' values, compares age and prints output

--- ex01/main.c
+++ ex01/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // csv columns: ID (null?) - NAME - AGE
 
@@ -17,21 +18,39 @@ int main(){
     // it's initial value is 1 byte because each char will be read
     // individually
     int contentSize = 0;
-    char *content;
+    char *content = (char *)malloc(0);
 
+    // temporary/auxiliar variable to read individual chars
     char tmp;
     
     // loop to read each of the file's characters
     while(fread(&tmp, sizeof(char), 1, fp)){
         content = (char *)realloc(content, ++contentSize * sizeof(char));
-        content[contentSize] = tmp;
-        printf("%c", tmp);
+        content[contentSize-1] = tmp;
+    }
+    
+    // temporary/auxiliar variables to read each .csv row
+    // and corresponding csvs
+    char *tmp_row, *tmp_id, *tmp_name; 
+    int tmp_age;
+
+    // deletes the header row (first register)
+    if(content){ strsep(&content, "\n"); }
+
+    // loops through every row (every register)
+    while(content){
+        // gets next row
+        tmp_row = strsep(&content, "\n");
+        
+        // gets each of the row's values
+        tmp_id = strsep(&tmp_row, ", ");
+        tmp_name = strsep(&tmp_row, ", ");
+        tmp_age = atoi(strsep(&tmp_row, ", "));
+        
+        // if that row's user is overage, prints output
+        if(tmp_age >= 18){ strcmp(tmp_name, "") ? printf("O usuario %s eh de maior.\n", tmp_name) : printf("O usuario de identificacao %s eh de maior.\n", tmp_id); }
     }
 
-    // terminates the string
-    content = (char *)realloc(content, ++contentSize * sizeof(char));
-    content[contentSize-1] = '\0';
-    
     // frees allocated memory
     fclose(fp);
     free(content);
